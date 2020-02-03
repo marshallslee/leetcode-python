abe84c82d4ac62fb356520bf71b3fb7d39e8f615
Marshall Lee <marshall.s.lee@gmail.com>
2020-02-03 11:12:36 +0900

    DATE: February 3, 2020 (MON)

--- pow.py
+++ pow.py
@@ -1,25 +1,31 @@
-# DATE: January 22, 2020 (WED)
+# DATE: February 3, 2020 (MON)
 # Source: https://leetcode.com/problems/powx-n/
 
 
 class Solution:
-    def myPow(self, x: float, n: int) -> float:
-        result = 1
-        if x > 0:
-            for i in range(n):
-                result *= x
-        elif x == 0:
-            result = 1
+    def my_pow(self, x: float, n: int) -> float:
+        if -100.0 < x < 100.0:
+            if n == 0:
+                return 1
+
+            if n % 2 == 0:
+                tmp = self.my_pow(x, n / 2)
+                return tmp * tmp
+
+            if n > 0:
+                return x * self.my_pow(x, n - 1)
+
+            # if n is positive, return x * x^(n+1)
+            return 1.0 / x * self.my_pow(x, n + 1)
         else:
-            n *= -1
-            for i in range(n):
-                result //= x
-                print(result)
-        return format(result, '.5f')
+            print('unable to calculate. x must be between -100.0 and 100.0.')
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.myPow(2.0000, 10))
-    print(s.myPow(2.10000, 0))
-    print(s.myPow(2.0000, -3))
+    print(s.my_pow(2.0000, 10))  # Expects 1024
+    print(s.my_pow(2.10000, 0))  # Expects 1
+    print(s.my_pow(2.1, 3))
+    print(s.my_pow(2.0000, -3))  # Expects 0.125
+    print(s.my_pow(-10, 2))  # Expects 100
+
