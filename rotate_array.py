5ba0ec6e3914cfe529d5b9851232b40096f81d47
Marshall Lee <marshall.s.lee@gmail.com>
2020-02-04 15:48:51 +0900

    DATE: February 4, 2020 (TUE)

---
+++ rotate_array.py
@@ -0,0 +1,24 @@
+def rotate(nums, k):
+    tmp_arr = [None] * len(nums)
+    for i in range(len(nums)):
+        tmp_arr[(i+k) % len(nums)] = nums[i]
+
+    for j in range(len(nums)):
+        nums[j] = tmp_arr[j]
+    print(nums)
+    return
+
+
+def run():
+    arr1 = [1, 2, 3, 4, 5, 6, 7]
+    i = 3
+    rotate(arr1, i)
+
+    arr2 = [1, 2]
+    j = 1
+    rotate(arr2, j)
+    return
+
+
+if __name__ == '__main__':
+    run()
